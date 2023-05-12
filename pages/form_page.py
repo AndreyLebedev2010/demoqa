8b572c9204e211f30d54489d96793d4721436399
LebedevAndrey <LebedevAndrey2010@mail.ru>
2023-05-12 18:19:55 +0300

    first commit

---
+++ pages/form_page.py
@@ -0,0 +1,9 @@
+from components.components import WebElement
+from pages.base_page import BasePage
+
+
+class FormPage(BasePage):
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/automation-practice-form'
+
+        super().__init__(driver, self.base_url)
